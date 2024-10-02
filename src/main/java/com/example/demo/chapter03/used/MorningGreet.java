53627f136e9d1b423836944659f24a2287f3eb33
ShamNeko <mkdvlp01@gmail.com>
2024-10-02 23:51:27 +0900

    [WIP]MornigGreetとEveningGreet作成中

---
+++ src/main/java/com/example/demo/chapter03/used/MorningGreet.java
@@ -0,0 +1,11 @@
+package com.example.demo.chapter03.used;
+
+public class MorningGreet implements Greet {
+
+    @Override
+    public void greeting() {
+        // TODO 自動生成されたメソッド・スタブ
+
+    }
+
+}
